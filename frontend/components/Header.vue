b2c63390aefa26fc4762cdce515a21a26f2027a9
ondghoskot <anaschtioui65@gmail.com>
2024-10-15 19:56:56 +0100

    progress: add pages/components and configure nuxt

---
+++ frontend/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <header>
+      <nav class="navbar">
+        <div class="logo">
+          <nuxt-link to="/">Exo Master</nuxt-link>
+        </div>
+        <div class="auth-buttons">
+          <div v-if="!user">
+            <nuxt-link to="/login">Login</nuxt-link>
+            <nuxt-link to="/signup">Sign Up</nuxt-link>
+          </div>
+          <div v-else>
+            <nuxt-link to="/account">Account</nuxt-link>
+          </div>
+        </div>
+      </nav>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        user: null,
+      }
+    },
+    mounted() {
+      this.user = localStorage.getItem("user") || null;
+    }
+  }
+  </script>
+  
+  <style>
+  </style>
+  
---
+++ frontend/pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+      <Header />
+  
+      <div class="hero">
+        <h1>Select a Muscle Group</h1>
+      </div>
+  
+      <div class="muscle-groups">
+        <div v-for="group in muscleGroups" :key="group" class="muscle-card">
+          <nuxt-link :to="`/exercises/${group}`">{{ group }}</nuxt-link>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import Header from '~/components/Header.vue'
+  
+  export default {
+    components: {
+      Header
+    },
+    data() {
+      return {
+        muscleGroups: [
+          'Chest', 'Back', 'Legs', 'Arms', 'Shoulders', 'Core'
+        ]
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .hero {
+    text-align: center;
+    margin: 20px 0;
+  }
+  .muscle-groups {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+  .muscle-card {
+    margin: 10px;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+  }
+  </style>
+  
